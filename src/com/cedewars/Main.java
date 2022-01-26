a8e0d13f7f116ba04cbcd651bbb2710f75be54e0
Tomek Jaworski <tj.toja@gmai.com>
2022-01-26 12:34:56 +0100

    Zadanie 3

--- src/com/cedewars/Main.java
+++ src/com/cedewars/Main.java
@@ -6,5 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println(check("The quick brown fox jumps over the lazy dog."));
+        System.out.println(check("You shall not pass!"));
+    }
+
+    public static boolean check(String sentence) {
+        String abc = "abcdefghijklmnopqrstuwxyz";
+        sentence = sentence.toLowerCase();
+        for (int i = 0; i < abc.length(); i++) {
+            if (sentence.indexOf(abc.charAt(i))==-1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
