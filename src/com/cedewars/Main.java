7322de1227170f30611a6b5cd243f83f5029bafb
Tomek Jaworski <tj.toja@gmai.com>
2022-01-27 13:34:00 +0100

    Zadanie 7

--- src/com/cedewars/Main.java
+++ src/com/cedewars/Main.java
@@ -1,25 +1,25 @@
 package com.cedewars;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
-        int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
-        System.out.println(comp(a,b));
+        ArrayList<int[]> list = new ArrayList<int[]>();
+        list.add(new int[] {10,0});
+        list.add(new int[] {3,5});
+        list.add(new int[] {2,5});
+        countPassengers(list);
     }
 
-    public static boolean comp(int[] a, int[] b) {
-        if ((a==null)||(b==null)||(a.length==0)||(b.length==0)){
-            return false;
+    public static int countPassengers(ArrayList<int[]> stops) {
+        //Code here!
+        int passNumber = 0;
+        for (int [] ints : stops) {
+                passNumber = passNumber + ints[0] - ints[1];
         }
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int[] aa = Arrays.stream(a).map(n -> n*n).toArray();
-
-        return Arrays.equals(aa,b);
+        return passNumber;
     }
 
 }
