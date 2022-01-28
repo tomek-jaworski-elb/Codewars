da19eb4cfeb63a8748dbc8c3291fde70ba39ece6
Tomek Jaworski <tj.toja@gmai.com>
2022-01-28 07:30:38 +0100

    Zadanie 9

--- src/com/cedewars/Main.java
+++ src/com/cedewars/Main.java
@@ -1,58 +1,37 @@
 package com.cedewars;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        pigIt("Pig latin is cool");
-        System.out.println("\n");
-        pigIt("This is my string");
+        System.out.println(createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+
+
     }
 
-    public static String pigIt(String str) {
-        // Write code here
-
-        String[] sliptedString = str.split(" ");
-        List<String> strings = new ArrayList<>(Arrays.asList(sliptedString));
-        for (int i = 0; i < strings.size(); i++) {
-            strings.set(i, shiftLeft(strings.get(i).toCharArray()) + "yl");
-        }
-        for (String s : strings) {
-            System.out.println(s);
-        }
-
-        return null;
-    }
-
-    public static String shiftRight(char[] letters) {
-
-        char last = letters[letters.length - 1];          // save off first element
-
-        // shift right
-        for (int index = letters.length - 2; index >= 0; index--)
-            letters[index + 1] = letters[index];
-
-        // wrap last element into first slot
-        letters[0] = last;
-        return Arrays.toString(letters);
-    }
-
-    public static String shiftLeft(char[] letters) {
-        int shiftBy = letters.length - 1;
-
-        for (int i = 0; i < shiftBy; i++) {
-            for (int j = letters.length - 1; j > 0; j--) {
-                char temp = letters[j];
-                letters[j] = letters[j - 1];
-                letters[j - 1] = temp;
+    public static String createPhoneNumber(int[] numbers) {
+        // Your code here!
+        StringBuilder result = new StringBuilder("(");
+        for (int i = 0; i < numbers.length; i++) {
+            if (i < 2) {
+                result.append(numbers[i]);
+            }
+            if (i == 2) {
+                result.append(numbers[i]).append(") ");
+            }
+            if ((i >= 3) && (i < 5)) {
+                result.append(numbers[i]);
+            }
+            if (i == 5) {
+                result.append(numbers[i]).append("-");
+            }
+            if (i>5) {
+                result.append(numbers[i]);
             }
         }
-
-        return String.valueOf(letters);
+        return result.toString();
     }
+
 
 }
