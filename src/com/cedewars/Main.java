dfc2e4c27643ee3aac042e7ac0e608bd445fe874
Tomek Jaworski <tj.toja@gmai.com>
2022-01-28 11:58:50 +0100

    Zadanie 13

--- src/com/cedewars/Main.java
+++ src/com/cedewars/Main.java
@@ -1,35 +1,16 @@
 package com.cedewars;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        String s = "abcdef";
-        String s1 = "HelloWorld";
-        String s2 = "LovePizza";
-        System.out.println(Arrays.toString(solution(s)));
-        System.out.println(Arrays.toString(solution(s1)));
-        System.out.println(Arrays.toString(solution(s2)));
+
+        System.out.println(countBits(1234));
 
     }
 
-    public static String[] solution(String s) {
-        //Write your code here
-        String[] result = null;
-        if (s.length() % 2 == 0) {
-            result = new String[s.length() / 2];
-        } else {
-            result = new String[(s.length() / 2) + 1];
-        }
-        for (int i = 0; i < s.length(); i = i + 2) {
-            try {
-                result[i / 2] = Character.toString(s.charAt(i)) + Character.toString(s.charAt(i + 1));
-            } catch (IndexOutOfBoundsException e) {
-                result[i / 2] = Character.toString(s.charAt(i)) + Character.toString('_');
-            }
-        }
-        return result;
+    public static int countBits(int n){
+        // Show me the code!
+        return Integer.bitCount(n);;
     }
 }
