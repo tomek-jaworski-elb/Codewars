aa8f489e536241fa19c51ecf9b5e9afb26eaa570
Tomek Jaworski <tj.toja@gmai.com>
2022-01-28 13:43:29 +0100

    Zadanie 15

--- src/com/cedewars/Main.java
+++ src/com/cedewars/Main.java
@@ -1,34 +1,46 @@
 package com.cedewars;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println(findIt(new int[]{20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5}));
+        System.out.println(Arrays.toString(deleteNth(new int[]{1, 1, 3, 3, 7, 2, 2, 2, 2}, 3)));
+        System.out.println(Arrays.toString(deleteNth(new int[]{1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1}, 2)));
 
     }
 
-    public static int findIt(int[] a) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int odd = 0;
-        for (int i : a) {
-            int val=0;
-            try {
-                val= map.get(i);
-            } catch (NullPointerException e) {
-                map.put(i,1);
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+        //Code here ;)
+        if (elements.length > 0) {
+            Map<Integer, Integer> map = new LinkedHashMap<>();
+            int count = 0;
+            for (int element : elements) {
+                try {
+                    if (map.get(element) <= maxOccurrences) {
+                        count = map.get(element) + 1;
+                    }
+                } catch (NullPointerException e) {
+                    count = 1;
+                }
+                map.put(element, count);
+                count = 0;
             }
-            map.put(i,val+1);
-        }
-        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
-            if (entry.getValue()%2==1) {
-                return entry.getKey();
-            }
-        }
-        return odd;
-    }
+            List<Integer> integerArrays = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                for (int i = 0; i < entry.getValue(); i++) {
+                    integerArrays.add(entry.getKey());
+                }
 
+            }
+            int[] result = new int[integerArrays.size()];
+            for (int i = 0; i < integerArrays.size(); i++) {
+                result[i] = integerArrays.get(i);
+            }
+            return result;
+        } else {
+            return new int[0];
+        }
+    }
 }
