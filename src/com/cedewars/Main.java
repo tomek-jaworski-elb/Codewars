175c0852c14ef19de4889b803bf7cbe5379f5429
Tomek Jaworski <tj.toja@gmai.com>
2022-01-28 07:15:30 +0100

    Zadanie 8

--- src/com/cedewars/Main.java
+++ src/com/cedewars/Main.java
@@ -1,25 +1,58 @@
 package com.cedewars;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        ArrayList<int[]> list = new ArrayList<int[]>();
-        list.add(new int[] {10,0});
-        list.add(new int[] {3,5});
-        list.add(new int[] {2,5});
-        countPassengers(list);
+        pigIt("Pig latin is cool");
+        System.out.println("\n");
+        pigIt("This is my string");
     }
 
-    public static int countPassengers(ArrayList<int[]> stops) {
-        //Code here!
-        int passNumber = 0;
-        for (int [] ints : stops) {
-                passNumber = passNumber + ints[0] - ints[1];
+    public static String pigIt(String str) {
+        // Write code here
+
+        String[] sliptedString = str.split(" ");
+        List<String> strings = new ArrayList<>(Arrays.asList(sliptedString));
+        for (int i = 0; i < strings.size(); i++) {
+            strings.set(i, shiftLeft(strings.get(i).toCharArray()) + "yl");
         }
-        return stops.stream().mapToInt(n -> n[0]-n[1]).sum();
+        for (String s : strings) {
+            System.out.println(s);
+        }
+
+        return null;
+    }
+
+    public static String shiftRight(char[] letters) {
+
+        char last = letters[letters.length - 1];          // save off first element
+
+        // shift right
+        for (int index = letters.length - 2; index >= 0; index--)
+            letters[index + 1] = letters[index];
+
+        // wrap last element into first slot
+        letters[0] = last;
+        return Arrays.toString(letters);
+    }
+
+    public static String shiftLeft(char[] letters) {
+        int shiftBy = letters.length - 1;
+
+        for (int i = 0; i < shiftBy; i++) {
+            for (int j = letters.length - 1; j > 0; j--) {
+                char temp = letters[j];
+                letters[j] = letters[j - 1];
+                letters[j - 1] = temp;
+            }
+        }
+
+        return String.valueOf(letters);
     }
 
 }
